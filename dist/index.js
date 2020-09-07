cebf770e43c0f73b054bf9b9d435bb2024d8a188
Alfonso Reyes <arc980103@gmail.com>
2020-09-07 17:49:08 -0600

    build: update dist file

--- dist/index.js
+++ dist/index.js
@@ -6006,17 +6006,35 @@ defineProperty(
 /***/ 351:
 /***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
 
+/**
+ * Add required imports
+ */
 const core = __webpack_require__(186);
 const github = __webpack_require__(438);
-
-const io_user = core.getInput("io_user");
-const io_key = core.getInput("io_key");
-const blink = core.getInput("blink");
-const time = core.getInput("time");
-
 const MQTT = __webpack_require__(923);
 
-const topic = `mxarc/feeds/issuetron/json`;
+// GitHub Action inputs
+const io_user = core.getInput('io_user');
+const io_key = core.getInput('io_key');
+const blink = core.getInput('blink');
+const time = core.getInput('time');
+
+// Required env vars
+const required_vars = ['IO_USER', 'IO_KEY', 'GITHUB_EVENT_PATH'];
+
+required_vars.forEach((env) => {
+  if (!process.env[env] || !process.env[env].length) {
+    console.error(
+      `Env var ${env} is not defined. Please check your repo secrets`
+    );
+    process.exit(1);
+  }
+});
+
+const eventContent = fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8');
+
+console.log(eventContent);
+/*const topic = `mxarc/feeds/issuetron/json`;
 
 (async () => {
   const client = await MQTT.connectAsync("mqtt://io.adafruit.com", {
@@ -6025,21 +6043,18 @@ const topic = `mxarc/feeds/issuetron/json`;
     password: io_key,
   });
   try {
-    console.log("Publishing data...");
+    core.info("Sending call to IoT device...");
     await client.publish(topic, JSON.stringify({ blink, time }));
     await client.end();
-    // This line doesn't run until the client has disconnected without error
-    console.log("Published data!");
-    console.log("Done");
+    core.info("Message sent!");
+    core.info("Done");
     core.setOutput("success", "true");
   } catch (error) {
-    // just a stub while I work on the device code
     core.setOutput("success", "false");
-    // Do something about it!
-    console.log(error.stack);
+    core.info(error.stack);
     process.exit();
   }
-})();
+})();*/
 
 
 /***/ }),
