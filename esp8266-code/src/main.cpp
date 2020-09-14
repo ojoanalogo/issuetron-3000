f3b7a8a7abd09641df361138db0d6500802b5468
Alfonso Reyes <arc980103@gmail.com>
2020-09-14 14:52:25 -0600

    feat: add lcd code

--- esp8266-code/src/main.cpp
+++ esp8266-code/src/main.cpp
@@ -2,6 +2,8 @@
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
 #include <ArduinoJson.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
 // the led output pin
 #define LED 5
@@ -16,6 +18,10 @@
 #define AIO_USERNAME "<your_username>"
 #define AIO_KEY "<password>"
 
+// set the LCD number of columns and rows
+int lcdColumns = 16;
+int lcdRows = 2;
+
 // Create an ESP8266 WiFiClient class to connect to the MQTT server.
 WiFiClient client;
 // or... use WiFiFlientSecure for SSL
@@ -29,6 +35,9 @@ Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO
 */
 Adafruit_MQTT_Subscribe issuetron = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/issuetron");
 
+// LCD declaration
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
+
 // Bug workaround for Arduino 1.6.6, it seems to need a function declaration
 // for some reason (only affects ESP8266, likely an arduino-builder bug).
 void MQTT_connect();
@@ -39,10 +48,18 @@ void setup()
   delay(10);
   pinMode(D1, OUTPUT);
   digitalWrite(D1, LOW);
+  lcd.init(); // initialize the lcd
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Starting...");
   Serial.println(F("Issuetron starting...\n\n"));
   // Connect to WiFi access point
+  lcd.clear();
+  lcd.print("Connecting to");
   Serial.print("Connecting to: ");
+  lcd.setCursor(0, 1);
   Serial.println(WLAN_SSID);
+  lcd.print(WLAN_SSID);
   // Begin WiFi connection attempt
   WiFi.begin(WLAN_SSID, WLAN_PASS);
   // Retry connection until WiFi is available
@@ -51,12 +68,18 @@ void setup()
     delay(500);
     Serial.print(".");
   }
+  lcd.clear();
   Serial.println();
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
+  lcd.setCursor(0, 0);
+  lcd.print("WiFi connected");
   Serial.println(WiFi.localIP());
   // Setup MQTT subscription for issuetron feed.
   mqtt.subscribe(&issuetron);
+  delay(1000);
+  lcd.clear();
+  lcd.print("Listening...");
 }
 
 void loop()
@@ -77,31 +100,46 @@ void loop()
     {
       Serial.print(F("Got Issuetron call: "));
       Serial.println((char *)issuetron.lastread);
-
+      lcd.setCursor(0, 0);
+      lcd.clear();
+      lcd.print("Issue opened!");
       // https://arduinojson.org/v6/assistant/
       const size_t capacity = JSON_OBJECT_SIZE(4) + 50;
       DynamicJsonDocument doc(capacity);
       // put lastread info into JSON string
       const char *json = (char *)issuetron.lastread;
-
       // call ArduinoJSON library
       deserializeJson(doc, json);
-
       // now we have access to the json keys and values
-
       bool blink = doc["blink"];        // true
       int time = doc["time"];           // 15
       const char *title = doc["title"]; // "Hello world 5.0"
       const char *user = doc["user"];   // "mxarc"
-
-      // Do the blink thingy
-      for (int i = 0; i < 5; i++)
+      if (title)
       {
-        delay(120);
-        digitalWrite(D1, HIGH);
-        delay(1300);
-        digitalWrite(D1, LOW);
+        delay(1000);
+        lcd.clear();
+        lcd.print("u:" + *user);
+        lcd.print("t:" + *title);
       }
+      // check time not less than one
+      if (time < 1)
+      {
+        time = 1;
+      }
+      // Do the blink thingy
+      if (blink)
+      {
+        for (int i = 0; i < time; i++)
+        {
+          delay(120);
+          digitalWrite(D1, HIGH);
+          delay(1000);
+          digitalWrite(D1, LOW);
+        }
+      }
+      delay(1500);
+      lcd.print("Listening...");
     }
   }
   // ping the server to keep the mqtt connection alive
